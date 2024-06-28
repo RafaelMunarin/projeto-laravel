bb5d5c3c1b8502c793aea0ca5013f30eac052307
RafaelMunarin <rafael.munarin@unidavi.edu.br>
2024-06-28 20:09:52 -0300

    teste alteraçaõ

--- meu_site/resources/views/home.blade.php
+++ meu_site/resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
-    <header>Lar das Meninas</header>
+    <header>Lar dos meninos</header>
     <nav>
         <ul>
             <li><a href="{{ route('home') }}">Home</a></li>
