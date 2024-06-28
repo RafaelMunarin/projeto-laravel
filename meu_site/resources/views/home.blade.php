d3e987e7124423713f2353824dd22932ed5da0f8
RafaelMunarin <rafael.munarin@unidavi.edu.br>
2024-06-28 20:14:33 -0300

    alterei novamente

--- meu_site/resources/views/home.blade.php
+++ meu_site/resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
-    <header>Lar dos meninos</header>
+    <header>Lar dos boys</header>
     <nav>
         <ul>
             <li><a href="{{ route('home') }}">Home</a></li>
