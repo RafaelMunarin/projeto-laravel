6c9011f9e0f225142c30c6cfa228c360dd8b7728
RafaelMunarin <rafael.munarin@unidavi.edu.br>
2024-06-28 21:36:33 -0300

    alterei o nome mais uma vez dnv

--- meu_site/resources/views/home.blade.php
+++ meu_site/resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
-    <header>Lar</header>
+    <header>Lar das Meninas</header>
     <nav>
         <ul>
             <li><a href="{{ route('home') }}">Home</a></li>
