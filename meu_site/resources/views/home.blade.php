973b1860c88405ed2660aa34ca3c23f68f993c1d
RafaelMunarin <rafael.munarin@unidavi.edu.br>
2024-06-28 21:39:20 -0300

    lar dos idosos
    aaaa

--- meu_site/resources/views/home.blade.php
+++ meu_site/resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
-    <header>Lar das Meninas</header>
+    <header>lar dos idosos</header>
     <nav>
         <ul>
             <li><a href="{{ route('home') }}">Home</a></li>
