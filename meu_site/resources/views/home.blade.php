dadfa339425e87cf6b1e4b10e121ca5e487b20a5
RafaelMunarin <rafael.munarin@unidavi.edu.br>
2024-06-28 21:40:58 -0300

    Update home.blade.php

--- meu_site/resources/views/home.blade.php
+++ meu_site/resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
-    <header>lar dos idosos</header>
+    <header>lar dos garotos</header>
     <nav>
         <ul>
             <li><a href="{{ route('home') }}">Home</a></li>
